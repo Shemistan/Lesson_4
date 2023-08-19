0d85f6729c8a5a90f6aeefb55778224897593a45
usernameBakhtiyor <harhil46@gmail.com>
2023-08-20 00:20:11 +0500

    Add

---
+++ reset.go
@@ -0,0 +1,13 @@
+package main
+
+func reset() {
+	food = 50
+	money = 100
+	husbandSatiation = 30
+	wifeSatiation = 30
+	husbandHappiness = 100
+	wifeHappy = 100
+	dirt = 0
+	shubaAmount = 0
+	totalConsumedMeal = 0
+}
